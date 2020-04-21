cafa6c71aca2cde36f8a0eabf0a31ac002140558
rymmx <rymmx@foxmail.com>
2020-04-21 21:04:52 +0800

    MA5, MA10,MA20

--- lib/ma_rymmx.py
+++ lib/ma_rymmx.py
@@ -1,9 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def ma(df):
+def ma(df=pd.DataFrame()):
     # MA5, MA10, MA20
-    df['MA5'] = df['close'][::-1].rolling(5).mean()
-    df['MA10'] = df['close'][::-1].rolling(10).mean()
-    df['MA20'] = df['close'][::-1].rolling(20).mean()
+    df['MA5'] = df['close'].rolling(5).mean()
+    df['MA10'] = df['close'].rolling(10).mean()
+    df['MA20'] = df['close'].rolling(20).mean()
+    # df['MA30'] = df['close'].rolling(30).mean()
+    # df['MA40'] = df['close'].rolling(40).mean()
 
     return df
+
+
+if __name__ == '__main__':
+    from settings import engine_ts
+    ts_code = "002480.SZ"
+    sql = "select trade_date,close from `daily_{}` order by trade_date DESC limit 100;".format(ts_code)
+    df = pd.read_sql_query(sql=sql, con=engine_ts, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
+    df = df[::-1]
+    print ma(df)
+
+    df.plot(x='trade_date', kind='line', figsize=(20, 8), grid=True)
+    plt.show()
