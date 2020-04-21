d56010011a95d77fb9f55222167e0bbf47781733
rymmx <rymmx@foxmail.com>
2020-04-21 21:17:48 +0800

    MA, MACD

--- lib/ma_rymmx.py
+++ lib/ma_rymmx.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 def ma(df=pd.DataFrame()):
-    # MA5, MA10, MA20
+    """MA5, MA10, MA20"""
     df['MA5'] = df['close'].rolling(5).mean()
     df['MA10'] = df['close'].rolling(10).mean()
     df['MA20'] = df['close'].rolling(20).mean()
@@ -20,6 +19,6 @@ if __name__ == '__main__':
     df = pd.read_sql_query(sql=sql, con=engine_ts, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
     df = df[::-1]
     print ma(df)
-
+    import matplotlib.pyplot as plt
     df.plot(x='trade_date', kind='line', figsize=(20, 8), grid=True)
     plt.show()
--- lib/macd_rymmx.py
+++ lib/macd_rymmx.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def macd(df=pd.DataFrame()):
-    """MACD"""
+    """MACD DIF DEA BAR"""
 
     df['ema12'] = ema(close=df['close'], length=12, offset=None)
     df['ema26'] = ema(close=df['close'], length=26, offset=None)
@@ -13,8 +13,16 @@ def macd(df=pd.DataFrame()):
     df['DEA'] = df['DIF'].rolling(9).mean()
     df['BAR'] = (df['DIF'] - df['DEA']) * 2
 
-    # MACD_judgment
-    # for i in df.iterrows():
-    #     df['MACD_judgment'] = 1 if (i['DIF'] - i['DEA']) > 0 else 0
-
     return df
+
+
+if __name__ == '__main__':
+    from settings import engine_ts
+    ts_code = "002480.SZ"
+    sql = "select trade_date,close from `daily_{}` order by trade_date DESC limit 100;".format(ts_code)
+    df = pd.read_sql_query(sql=sql, con=engine_ts, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
+    df = df[::-1]
+    print macd(df)
+    import matplotlib.pyplot as plt
+    df.plot(x='trade_date', kind='line', figsize=(20, 8), grid=True)
+    plt.show()
